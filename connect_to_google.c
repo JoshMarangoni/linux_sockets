bfd977772ceb49c812e3adec90b021798ab3aa01
Joshua Marangoni <joshua.marangoni@gmail.com>
2023-08-06 17:31:57 -0400

    comments for client

--- connect_to_google.c
+++ connect_to_google.c
@@ -1,24 +1,22 @@
-#include <stdio.h>
-#include <sys/socket.h>  // for socket()
-#include <netinet/in.h>  // for ??
-#include <arpa/inet.h>   // for ??
-#include <netdb.h>       // for ??
-#include <unistd.h>      // for ??
+#include <stdio.h>       // for printf
+#include <sys/socket.h>  // for socket
+#include <netdb.h>       // for gethostbyname
+#include <unistd.h>      // for close
 #include <string.h>      // for string manipulation
 
-// connect(), bind(), and accept() except pointers to
+// connect, bind, and accept except pointers to
 // a generic socket address (protocol independent).
 // use this type for casting
 typedef struct sockaddr SA;
 
 int main() {
-    // create a TCP socket file descriptor
+    // create an IPv4, TCP socket file descriptor
     int socketfd = socket(AF_INET, SOCK_STREAM, 0);
     if (socketfd < 0) {
         perror("socket error");
 	return 1;
     }
-    printf("client socket created successfully");
+    printf("client socket created successfully\r\n");
 
     // set up hostname
     const char* hostname = "google.com";
@@ -31,15 +29,14 @@ int main() {
 
     // set up sockaddr struct with server info
     struct sockaddr_in address;
-    address.sin_family = AF_INET;                         // ipv4
-    address.sin_port = htons(port);                       // server port
-    address.sin_addr.s_addr = *(in_addr_t*)host->h_addr;  // server ip
-
+    address.sin_family = AF_INET;   // ipv4
+    address.sin_port = htons(port); // server port, big endian
+    address.sin_addr.s_addr = *(in_addr_t*)host->h_addr; // server ip
 
     // attempt to establish a connection with the server
     // block until connection is established or an error occurs
     // if successful, open the client fd for reading and writing
-    int result = connect(socketfd, (struct sockaddr*)&address, sizeof address);
+    int result = connect(socketfd, (SA*)&address, sizeof(address));
     if (result < 0) {
         perror("connect error");
 	close(socketfd);
@@ -49,7 +46,7 @@ int main() {
 
     // send an HTTP GET request to server
     char* msg;
-    msg ="GET \\ HTTP/1.1\r\nHost:google.com\r\n\r\n";
+    msg ="GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n";
     send(socketfd, msg, strlen(msg), 0);
 
     // receive response from server
